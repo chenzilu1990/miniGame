ce0e6d593580f61e6a37ec49186ded3fb83a4b9f
e <2822459649@qq.com>
2018-05-13 15:49:37 +0800

    jwkhr

---
+++ game.js
@@ -0,0 +1,17 @@
+import './js/libs/weapp-adapter'
+import './js/libs/symbol'
+
+import Main from './js/main'
+
+new Main()
+
+// wx.onAccelerometerChange((x,y,z) => {
+//   console.log(x,y,z)
+// })
+
+// wx.startAccelerometer({
+//   success : () => {
+//     console.log('dfdf')
+//   }
+// })
+
